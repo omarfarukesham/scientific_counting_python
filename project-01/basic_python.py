d06ba44e045aa4984ced3f54186ab72479698061
omarfarukesham <omar.lu86@gmail.com>
2025-09-16 13:07:02 +0600

    Feat: function and loop is working

--- project-01/basic_python.py
+++ project-01/basic_python.py
@@ -1,21 +1,28 @@
 text = 'Hello Zaira'
 custom_key = 'python'
-print(text.lower().replace(' ', ''))
-
-def vigenere(message, key):
+def vigenere(message, key, direction):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
     for char in message.lower():
+    
         # Append space to the message
         if char == ' ':
             encrypted_text += char
-        else:
+        else:        
+            # Find the right key character to encode
             key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + key) % len(alphabet)
+            new_index = (index + offset*direction) % len(alphabet)
             encrypted_text += alphabet[new_index]
-   
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+    
+    return encrypted_text
+encryption = vigenere(text, custom_key, 1)
+print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
