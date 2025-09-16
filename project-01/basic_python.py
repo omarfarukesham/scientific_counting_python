c6f683c06b2ccc80406f4a087a4300b1c329ecc4
omarfarukesham <omar.lu86@gmail.com>
2025-09-16 12:38:46 +0600

    Feat: python learning with freecodecamp

---
+++ project-01/basic_python.py
@@ -0,0 +1,21 @@
+text = 'Hello Zaira'
+custom_key = 'python'
+print(text.lower().replace(' ', ''))
+
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:
+            key_char = key[key_index % len(key)]
+            index = alphabet.find(char)
+            new_index = (index + key) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+   
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
